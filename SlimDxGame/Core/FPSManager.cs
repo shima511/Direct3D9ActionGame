f0f0e25930bafd0f9bf602d10f7b6eff3086f079
Shima <wiicrew@mist.ocn.ne.jp>
2014-05-26 08:16:50 +0900

    * FPSマネージャクラスを追加

---
+++ SlimDxGame/Core/FPSManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlimDxGame.Core
+{
+    class FPSManager
+    {
+        private int time_begin = 0;
+        private int ideal_time = (int)(1000.0F / 60.0F);
+
+        public void Begin()
+        {
+            time_begin = Environment.TickCount;
+        }
+
+        public void End()
+        {
+            var past_time = Environment.TickCount - time_begin;
+            var wait_time = ideal_time - past_time;
+            if (wait_time > 0) System.Threading.Thread.Sleep(wait_time);
+        }
+    }
+}
