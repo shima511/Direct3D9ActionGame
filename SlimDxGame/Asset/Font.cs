69c2abfce55197bfae4452aa0bd8810f135f6489
Shima <wiicrew@mist.ocn.ne.jp>
2014-05-26 08:27:04 +0900

    * フォントクラス・ファクトリクラスの追加

---
+++ SlimDxGame/Asset/Font.cs
@@ -0,0 +1,15 @@
+﻿namespace SlimDxGame.Asset
+{
+    class Font : Base
+    {
+        public System.Drawing.Font Info { get; set; }
+        public SlimDX.Direct3D9.Font Resource { get; set; }
+        /// <summary>
+        /// リソースの解放
+        /// </summary>
+        public void Release()
+        {
+            Resource.Dispose();
+        }
+    }
+}
---
+++ SlimDxGame/AssetFactory/FontFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using SlimDX.Direct3D9;
+
+namespace SlimDxGame.AssetFactory
+{
+    class FontFactory
+    {
+        static public SlimDX.Direct3D9.Device Device { private get; set; }
+
+        static public Asset.Font CreateFont(System.Drawing.Font info){
+            var new_font = new Asset.Font();
+            var font_resource = new Font(Device, info);
+
+            new_font.Info = info;
+            new_font.Resource = font_resource;
+
+            return new_font;
+        }
+    }
+}
