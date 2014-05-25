9367dc75c90ece6617057b001562a66de67f8712
Shima <wiicrew@mist.ocn.ne.jp>
2014-05-26 08:29:47 +0900

    * シーンの基礎クラスを追加

---
+++ SlimDxGame/Scene/Base.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlimDxGame.Scene
+{
+    abstract class Base
+    {
+        public virtual void ExitScene(GameRootObjects root_objects)
+        {
+            root_objects.layers.Clear();
+            root_objects.update_list.Clear();
+            root_objects.input_manager.Clear();
+            root_objects.font_container.DeleteAllObject();
+            root_objects.tex_container.DeleteAllObject();
+            root_objects.sound_container.DeleteAllObject();
+            root_objects.model_container.DeleteAllObject();
+        }
+        public abstract int Update(GameRootObjects root, ref Base new_scene);
+    }
+}
