80c3a681ba697c876c15851ef63dd0db74a022ce
Shima <wiicrew@mist.ocn.ne.jp>
2014-05-26 08:20:06 +0900

    * オーディオデバイスを追加

---
+++ SlimDxGame/Device/Audio.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using SlimDX.XAudio2;
+using SlimDX.Multimedia;
+
+namespace SlimDxGame.Device
+{
+    class Audio : Device
+    {
+        public XAudio2 XAudioDevice { get; private set; }
+        private MasteringVoice xaMasterVoice;
+
+        public void Initialize()
+        {
+            XAudioDevice = new XAudio2();
+            xaMasterVoice = new MasteringVoice(XAudioDevice);
+        }
+
+        public void Terminate()
+        {
+            xaMasterVoice.Dispose();
+            XAudioDevice.Dispose();
+        }
+    }
+}
