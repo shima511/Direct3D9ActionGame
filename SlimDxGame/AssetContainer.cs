666c85d5a6616f996fe8c32074579d1c51e5e3aa
Shima <wiicrew@mist.ocn.ne.jp>
2014-05-26 08:28:25 +0900

    * アセットコンテナクラスを追加

---
+++ SlimDxGame/AssetContainer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace SlimDxGame
+{
+    class AssetContainer<TYPE> : Dictionary<string, TYPE>
+        where TYPE : Asset.Base
+    {
+        public void DeleteAllObject()
+        {
+            foreach (var obj in this.Values)
+            {
+                obj.Release();
+            }
+            this.Clear();
+        }
+
+        public bool IncludeInvalidObject(ref List<string> object_names)
+        {
+            bool include_invalid_object = false;
+            foreach(var obj in this){
+                if (obj.Value == null)
+                {
+                    object_names.Add(obj.Key + "\n");
+                    include_invalid_object = true;
+                }
+            }
+            return include_invalid_object;
+        }
+    }
+
+}
