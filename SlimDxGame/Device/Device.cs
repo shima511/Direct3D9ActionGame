2adeaf2c3ca45c2349949c880e8e634a52eb1a20
Shima <wiicrew@mist.ocn.ne.jp>
2014-05-26 08:18:48 +0900

    * デバイスインターフェースを追加

---
+++ SlimDxGame/Device/Device.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlimDxGame.Device
+{
+    interface Device
+    {
+        void Initialize();
+        void Terminate();
+    }
+
+}
