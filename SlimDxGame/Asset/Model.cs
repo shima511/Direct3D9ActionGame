b40181b36d0059fb1c54af4ca28f4ccf4f4ab915
Shima <wiicrew@mist.ocn.ne.jp>
2014-05-26 08:26:40 +0900

    * モデルクラス・ファクトリクラスを追加

---
+++ SlimDxGame/Asset/Model.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace SlimDxGame.Asset
+{
+    class Model : Base
+    {
+        public SlimDX.Direct3D9.Mesh Mesh { get; set; }
+        public List<SlimDX.Direct3D9.Texture> Textures { get; set; }
+        public List<SlimDX.Direct3D9.ExtendedMaterial> Materials { get; set; }
+
+        public Model()
+        {
+            Textures = new List<SlimDX.Direct3D9.Texture>();
+            Materials = new List<SlimDX.Direct3D9.ExtendedMaterial>();
+        }
+
+        /// <summary>
+        /// リソースの解放
+        /// </summary>
+        public void Release()
+        {
+            foreach (var tex in Textures)
+            {
+                tex.Dispose();
+            }
+            Mesh.Dispose();
+        }
+    }
+}
---
+++ SlimDxGame/AssetFactory/ModelFactory.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace SlimDxGame.AssetFactory
+{
+    class ModelFactory
+    {
+        static public SlimDX.Direct3D9.Device Device { private get; set; }
+
+        public static Asset.Model CreateModelFromFile(string filename)
+        {
+            var new_model = new Asset.Model();
+            new_model.Mesh = SlimDX.Direct3D9.Mesh.FromFile(Device, filename, SlimDX.Direct3D9.MeshFlags.Managed);
+            // 属性を取得
+            foreach (var material in new_model.Mesh.GetMaterials())
+            {
+                new_model.Materials.Add(material);
+            }
+
+            if (new_model.Materials.Count >= 1)
+            {
+                for (int i = 0; i < new_model.Materials.Count; i++)
+                {
+                    string baseDir = Path.GetDirectoryName(Application.ExecutablePath);
+                    var tex_filename = new_model.Materials[i].TextureFileName;
+                    if (!string.IsNullOrEmpty(tex_filename))
+                    {
+                        var tex_path = Path.Combine(baseDir, Path.Combine("models", tex_filename));
+                        new_model.Textures.Add(SlimDX.Direct3D9.Texture.FromFile(Device, tex_path));
+                    }
+                }
+            }
+
+            return new_model;
+        }
+    }
+}
