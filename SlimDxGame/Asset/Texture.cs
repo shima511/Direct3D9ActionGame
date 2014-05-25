441a03285476334c12aa62cec6c96b03227df961
Shima <wiicrew@mist.ocn.ne.jp>
2014-05-26 08:25:14 +0900

    * テクスチャクラス・テクスチャファクトリクラスを追加

---
+++ SlimDxGame/Asset/Texture.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace SlimDxGame.Asset
+{
+    class Texture : Base
+    {
+        public int Width { set; get; }
+        public int Height { set; get; }
+        public SlimDX.Direct3D9.Texture Resource { set; get; }
+        public void Release()
+        {
+            Resource.Dispose();
+        }
+    }
+}
---
+++ SlimDxGame/AssetFactory/TextureFactory.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using SlimDX.Direct3D9;
+
+namespace SlimDxGame.AssetFactory
+{
+    class TextureFactory
+    {
+        static public SlimDX.Direct3D9.Device device { private get; set; }
+
+        static public Asset.Texture CreateTextureFromFile(string filename)
+        {
+            var tex = new Asset.Texture();
+            try
+            {
+                ImageInformation info;
+                tex.Resource = Texture.FromFile(device, filename, 0, 0, 1, Usage.None, Format.A8R8G8B8, Pool.Managed, Filter.None, Filter.None, 0, out info);
+                var description = tex.Resource.GetSurfaceLevel(0).Description;
+                tex.Width = info.Width;
+                tex.Height = info.Height;
+            }catch(Direct3D9Exception ex){
+                System.Diagnostics.Debug.Assert(false, ex.Message + "：テクスチャ生成時");
+                tex = null;
+            }
+            return tex;
+        }
+
+        static public Asset.Texture CreateFromMemory(int width, int height, byte[] buffer)
+        {
+            var tex = new Asset.Texture();
+            tex.Height = height;
+            tex.Width = width;
+            tex.Resource = new Texture(device, width, height, 1, Usage.None, Format.A8R8G8B8, Pool.Managed);
+            var rect = tex.Resource.LockRectangle(0, LockFlags.None);
+            rect.Data.Write(buffer, 0, sizeof(byte) * buffer.Length);
+            tex.Resource.UnlockRectangle(0);
+            return tex;
+        }
+
+    }
+}
