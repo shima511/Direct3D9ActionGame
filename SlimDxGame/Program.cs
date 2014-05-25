ee30b380e99c380305ccf4bef67081c3eca4adf0
Shima <wiicrew@mist.ocn.ne.jp>
2014-05-26 08:15:16 +0900

    * メイン文の作成

---
+++ SlimDxGame/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SlimDxGame
+{
+    static class Program
+    {
+        /// <summary>
+        /// アプリケーションのメイン エントリ ポイントです。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            using (Core.Game game = new Core.Game(new Scene.Stage()))
+            {
+                game.Run();
+            }
+        }
+    }
+}
