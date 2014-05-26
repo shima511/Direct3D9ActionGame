8a0f867d1137564a8f06f1ccc2bc7e51b13b3aaf
Shima <wiicrew@mist.ocn.ne.jp>
2014-05-27 06:04:59 +0900

    * 衝突管理クラスを追加

---
+++ SlimDxGame/Collision/Manager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SlimDxGame.Collision
+{
+    class Manager : List<Object.ICollisionObject>, Component.IUpdateObject
+    {
+        public Object.Player Player { private get; set; }
+        public void Update()
+        {
+            Player.IsOnTheGround = false;
+            Player.IsInTheAir = true;
+            foreach (var obj in this)
+            {
+                obj.Hit(Player);
+            }
+        }
+    }
+}
