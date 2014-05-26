d476baf8ee675e1e255ce8277acb4842b03ba634
Shima <wiicrew@mist.ocn.ne.jp>
2014-05-27 05:55:35 +0900

    * カーソルクラスを追加

---
+++ SlimDxGame/Object/Cursor.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using SlimDX;
+
+namespace SlimDxGame.Object
+{
+    class Cursor : Base.Sprite, Component.IOperableObject, Component.IUpdateObject
+    {
+        public int NonActionTime { get; set; }
+        public int Index { get; set; }
+        public List<Vector2> Positions { get; set; }
+        public Action MoveAction { get; set; }
+
+        public void ControllerAction(SlimDxGame.Controller controller)
+        {
+            if (controller.DownButton.IsPressed())
+            {
+                MoveAction();
+                NonActionTime = 0;
+                Index++;
+            }
+            if (controller.UpButton.IsPressed())
+            {
+                MoveAction();
+                NonActionTime = 0;
+                Index--;
+            }
+        }
+
+        public void Update()
+        {
+            NonActionTime++;
+            if (Index == -1) Index += Positions.Count;
+            Index %= Positions.Count;
+            _position = Positions[Index];
+        }
+    }
+}
