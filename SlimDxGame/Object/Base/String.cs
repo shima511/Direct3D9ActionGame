8aba879d326167f83da9f5f54608174dd514f87d
Shima <wiicrew@mist.ocn.ne.jp>
2014-05-27 05:54:23 +0900

    * 描画用ストリングクラスを追加

---
+++ SlimDxGame/Object/Base/String.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using SlimDX;
+
+namespace SlimDxGame.Object.Base
+{
+    class String : PlaneObject
+    {
+        public Asset.Font Font { private get; set; }
+        public string Text { private get; set; }
+
+        private void UpdateMatrix()
+        {
+            var world_mat = CommonMatrix.WorldMatrix;
+            world_mat = Matrix.Identity;
+            world_mat = Matrix.Scaling(_scale.X, _scale.Y, 0) * Matrix.RotationZ(Angle);
+            world_mat.M41 = _position.X;
+            world_mat.M42 = _position.Y;
+            CommonMatrix.WorldMatrix = world_mat;
+        }
+
+        public override void Draw2D(SlimDX.Direct3D9.Sprite dev)
+        {
+            UpdateMatrix();
+            dev.Transform = CommonMatrix.WorldMatrix;
+            Font.Resource.DrawString(dev, Text, 0, 0, _color);
+        }
+    }
+}
