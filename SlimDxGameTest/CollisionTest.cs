a2647cba58b042257a2472d428b0b9a9d11ac459
Shima <wiicrew@mist.ocn.ne.jp>
2014-06-08 16:42:05 +0900

    * オブジェクトの初期化方法を変更 * 点と線分の衝突判定テストを追加

--- SlimDxGameTest/CollisionTest.cs
+++ SlimDxGameTest/CollisionTest.cs
@@ -10,14 +10,16 @@ namespace SlimDxGameTest
         [TestMethod]
         public void CircleHitCircle()
         {
-            SlimDxGame.Collision.Shape.Circle circle1 = new SlimDxGame.Collision.Shape.Circle();
-            SlimDxGame.Collision.Shape.Circle circle2 = new SlimDxGame.Collision.Shape.Circle();
-
-            circle1.Center = new Vector2(0.0f, 0.0f);
-            circle1.Radius = 1.0f;
-
-            circle2.Center = new Vector2(1.0f, 0.0f);
-            circle2.Radius = 1.0f;
+            SlimDxGame.Collision.Shape.Circle circle1 = new SlimDxGame.Collision.Shape.Circle()
+            {
+                Center = new Vector2(0.0f, 0.0f),
+                Radius = 1.0f
+            };
+            SlimDxGame.Collision.Shape.Circle circle2 = new SlimDxGame.Collision.Shape.Circle()
+            {
+                Center = new Vector2(1.0f, 0.0f),
+                Radius = 1.0f
+            };
 
             Assert.IsTrue(circle1.Hit(circle2));
         }
@@ -25,14 +27,16 @@ namespace SlimDxGameTest
         [TestMethod]
         public void CircleHitLine()
         {
-            SlimDxGame.Collision.Shape.Circle circle = new SlimDxGame.Collision.Shape.Circle();
-            SlimDxGame.Collision.Shape.Line line = new SlimDxGame.Collision.Shape.Line();
-
-            circle.Center = new Vector2(0.0f, 0.0f);
-            circle.Radius = 1.0f;
-
-            line.StartingPoint = new Vector2(1.0f, 1.0f);
-            line.TerminalPoint = new Vector2(-1.0f, -1.0f);
+            SlimDxGame.Collision.Shape.Circle circle = new SlimDxGame.Collision.Shape.Circle()
+            {
+                Center = new Vector2(0.0f, 0.0f),
+                Radius = 1.0f
+            };
+            SlimDxGame.Collision.Shape.Line line = new SlimDxGame.Collision.Shape.Line()
+            {
+                StartingPoint = new Vector2(1.0f, 1.0f),
+                TerminalPoint = new Vector2(-1.0f, -1.0f)
+            };
 
             Assert.IsTrue(circle.Hit(line));
         }
@@ -40,14 +44,16 @@ namespace SlimDxGameTest
         [TestMethod]
         public void CircleDoesNotHitLine()
         {
-            SlimDxGame.Collision.Shape.Circle circle = new SlimDxGame.Collision.Shape.Circle();
-            SlimDxGame.Collision.Shape.Line line = new SlimDxGame.Collision.Shape.Line();
-
-            circle.Center = new Vector2(0.0f, 0.0f);
-            circle.Radius = 1.0f;
-
-            line.StartingPoint = new Vector2(-5.0f, 0.5f);
-            line.TerminalPoint = new Vector2(5.0f, 0.5f);
+            SlimDxGame.Collision.Shape.Circle circle = new SlimDxGame.Collision.Shape.Circle()
+            {
+                Center = new Vector2(0.0f, 0.0f),
+                Radius = 1.0f
+            };
+            SlimDxGame.Collision.Shape.Line line = new SlimDxGame.Collision.Shape.Line()
+            {
+                StartingPoint = new Vector2(-5.0f, 0.5f),
+                TerminalPoint = new Vector2(5.0f, 0.5f)
+            };
 
             Assert.IsFalse(circle.Hit(line));
         }
@@ -55,16 +61,52 @@ namespace SlimDxGameTest
         [TestMethod]
         public void CircleDoesNotHitCircle()
         {
-            SlimDxGame.Collision.Shape.Circle circle1 = new SlimDxGame.Collision.Shape.Circle();
-            SlimDxGame.Collision.Shape.Circle circle2 = new SlimDxGame.Collision.Shape.Circle();
-
-            circle1.Center = new Vector2(0.0f, 0.0f);
-            circle1.Radius = 1.0f;
-
-            circle2.Center = new Vector2(3.0f, 0.0f);
-            circle2.Radius = 1.0f;
+            SlimDxGame.Collision.Shape.Circle circle1 = new SlimDxGame.Collision.Shape.Circle()
+            {
+                Center = new Vector2(0.0f, 0.0f),
+                Radius = 1.0f
+            };
+            SlimDxGame.Collision.Shape.Circle circle2 = new SlimDxGame.Collision.Shape.Circle()
+            {
+                Center = new Vector2(3.0f, 0.0f),
+                Radius = 1.0f
+            };
 
             Assert.IsFalse(circle1.Hit(circle2));
+        }
+
+        [TestMethod]
+        public void PointHitLine()
+        {
+            SlimDxGame.Collision.Shape.Point point = new SlimDxGame.Collision.Shape.Point()
+            {
+                Position = new Vector2(0.0f, 0.0f)
+            };
+            SlimDxGame.Collision.Shape.Line line = new SlimDxGame.Collision.Shape.Line()
+            {
+                StartingPoint = new Vector2(-1.0f, 1.0f),
+                TerminalPoint = new Vector2(1.0f, -1.0f)
+            };
+
+            Assert.IsTrue(point.Hit(line));
+            Assert.IsTrue(line.Hit(point));
+        }
+
+        [TestMethod]
+        public void PointDoesNotHitLine()
+        {
+            SlimDxGame.Collision.Shape.Point point = new SlimDxGame.Collision.Shape.Point()
+            {
+                Position = new Vector2(0.0f, 0.0f)
+            };
+            SlimDxGame.Collision.Shape.Line line = new SlimDxGame.Collision.Shape.Line()
+            {
+                StartingPoint = new Vector2(1.0f, 1.0f),
+                TerminalPoint = new Vector2(1.0f, -1.0f)
+            };
+
+            Assert.IsFalse(point.Hit(line));
+            Assert.IsFalse(line.Hit(point));
         }
     }
 }
