98a6a5e6a4744e5bc00534d1d79962f9feb1454b
Shima <wiicrew@mist.ocn.ne.jp>
2014-06-08 23:10:47 +0900

    * FPSを描画するようにした

--- SlimDxGame/Core/FPSManager.cs
+++ SlimDxGame/Core/FPSManager.cs
@@ -8,18 +8,37 @@ namespace SlimDxGame.Core
 {
     class FPSManager
     {
-        private int time_begin = 0;
-        private int ideal_time = (int)(1000.0F / 60.0F);
+        float fps = 0;
+        int count = 0;
+        int time_begin = 0;
+        int ideal_time = (int)(1000.0F / 60.0F);
+        const int Sample = 60;
 
         public void Begin()
         {
-            time_begin = Environment.TickCount;
+            if (count == 0)
+            {
+                time_begin = Environment.TickCount;
+            }
+            if (count == Sample)
+            {
+                var t = Environment.TickCount;
+                fps = 1000.0f/((t-time_begin)/(float)Sample);
+                count = 0;
+                time_begin = t;
+            }
+            count++;
+        }
+
+        public void Draw(SlimDX.Direct3D9.Sprite dev, SlimDX.Direct3D9.Font font)
+        {
+            font.DrawString(dev, fps.ToString(), 0, 0, System.Drawing.Color.White);
         }
 
         public void End()
         {
             var past_time = Environment.TickCount - time_begin;
-            var wait_time = ideal_time - past_time;
+            var wait_time = count * ideal_time - past_time;
             if (wait_time > 0) System.Threading.Thread.Sleep(wait_time);
         }
     }
