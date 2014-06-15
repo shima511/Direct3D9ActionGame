7956ec3381855413d5e0cca70d7ab4bef3ced934
Shima <wiicrew@mist.ocn.ne.jp>
2014-06-15 18:17:56 +0900

    * 衝突判定を有する地形をロードできるようにした

--- SlimDxGame/StageLoader.cs
+++ SlimDxGame/StageLoader.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.VisualBasic.FileIO;
 
 namespace SlimDxGame
 {
-
     class StageLoader
     {
         /// <summary>
@@ -15,9 +15,9 @@ namespace SlimDxGame
         public bool LoadFailed { get; private set; }
 
         /// <summary>
-        /// 衝突判定が存在する静的なオブジェクトを取得します
+        /// 衝突判定が存在する地形オブジェクトを取得します
         /// </summary>
-        public List<Object.ICollisionObject> Collisions { get; private set; }
+        public List<Object.Ground.Base> GroundCollisions { get; private set; }
 
         /// <summary>
         /// プレイヤーの初期位置を取得します
@@ -26,34 +26,49 @@ namespace SlimDxGame
 
         public StageLoader()
         {
-            Collisions = new List<Object.ICollisionObject>();
+            GroundCollisions = new List<Object.Ground.Base>();
         }
 
-        Object.ICollisionObject CreateCollision(string[] values)
+        void AttachGroundProperty(string str, out Object.Ground.Base ground)
         {
-            Object.ICollisionObject new_collision = null;
-            switch (values[4])
+            switch (str)
             {
                 case "Floor":
-                    new_collision = new Object.Floor();
+                    ground = new Object.Ground.Floor();
                     break;
                 case "Ceiling":
-                    new_collision = new Object.Ceiling();
+                    ground = new Object.Ground.Ceiling();
                     break;
                 case "RightWall":
-                    new_collision = new Object.RightWall();
+                    ground = new Object.Ground.RightWall();
                     break;
                 case "LeftWall":
-                    new_collision = new Object.LeftWall();
+                    ground = new Object.Ground.LeftWall();
                     break;
                 default:
+                    ground = null;
                     break;
             }
-            return new_collision;
+        }
+
+        void SetGroundCollisionLine(string[] values, Object.Ground.Base ground)
+        {
+            var start_point = new SlimDX.Vector2(float.Parse(values[0]), float.Parse(values[1]));
+            var terminal_point = new SlimDX.Vector2(float.Parse(values[2]), float.Parse(values[3]));
+            ground.CollisionLine.StartingPoint = start_point;
+            ground.CollisionLine.TerminalPoint = terminal_point;
+        }
+
+        Object.Ground.Base CreateGroundCollision(string[] values)
+        {
+            Object.Ground.Base new_ground;
+            AttachGroundProperty(values[4], out new_ground);
+            SetGroundCollisionLine(values, new_ground);
+            return new_ground;
         }
 
         /// <summary>
-        /// 
+        /// ステージを読み込みます
         /// </summary>
         /// <param name="filename">ファイル名</param>
         public void Load(string filename)
@@ -61,19 +76,24 @@ namespace SlimDxGame
             LoadFailed = false;
             try
             {
-                using (var sr = new System.IO.StreamReader(filename))
+                using (var parser = new TextFieldParser(filename, System.Text.Encoding.GetEncoding("Shift_JIS")))
                 {
-                    while (!sr.EndOfStream)
+                    parser.TextFieldType = FieldType.Delimited;
+
+                    parser.SetDelimiters(",");
+                    while (!parser.EndOfData)
                     {
-                        var line = sr.ReadLine();
-                        var values = line.Split(',');
-                        var new_collision = CreateCollision(values);
-                        Collisions.Add(new_collision);
+                        string[] row = parser.ReadFields();
+                        var new_ground = CreateGroundCollision(row);
+                        GroundCollisions.Add(new_ground);
                     }
                 }
             }
-            catch(System.Exception){
+            catch(System.Exception e){
                 LoadFailed = true;
+#if DEBUG
+                System.Windows.Forms.MessageBox.Show(e.Message);
+#endif
             }
         }
     }
