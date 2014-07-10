a7b75c6a45067e193476bbd3838841198801fa51
Shima <wiicrew@mist.ocn.ne.jp>
2014-07-10 21:56:08 +0900

    * 1ステージのオブジェクトをまとめた構造体を追加

---
+++ BinaryParser/Objects.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryParser
+{
+    public struct Objects
+    {
+        public List<Property.Collision> Collisions { get; set; }
+        public List<Property.Item> Items { get; set; }
+        public List<Property.Decolation> Decolations { get; set; }
+        public List<Property.Enemy> Enemies { get; set; }
+        public Property.Stage Stage { get; set; }
+        public Property.Player Player { get; set; }
+    }
+}
