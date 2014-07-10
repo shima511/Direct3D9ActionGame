5438b32bef869335e8c650228458d7cba80fcf6e
Shima <wiicrew@mist.ocn.ne.jp>
2014-07-10 21:54:02 +0900

    * バイナリの読み書きクラスを追加

--- BinaryParser/Reader.cs
+++ BinaryParser/Reader.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace BinaryParser
 {
-    public class Class1
+    public class Reader
     {
     }
 }
---
+++ BinaryParser/Writer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace BinaryParser
+{
+    public class Writer
+    {
+        List<byte> byteArray = new List<byte>();
+
+
+        public void Write(string filename, Objects objects)
+        {
+            BinaryWriter writer = new BinaryWriter(File.OpenRead(filename));
+            writer.Write(byteArray.ToArray());
+        }
+    }
+}
