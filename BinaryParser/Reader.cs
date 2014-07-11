405d203a81e07d9d18db15ec7e1cff5dbe83f87d
Shima <wiicrew@mist.ocn.ne.jp>
2014-07-11 09:16:01 +0900

    * 読み込みクラスを実装した

--- BinaryParser/Reader.cs
+++ BinaryParser/Reader.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Runtime.InteropServices;
 
 namespace BinaryParser
 {
@@ -42,27 +43,84 @@ namespace BinaryParser
 
         void ReadPlayerData(ref Objects objects)
         {
-            byte[] bytes = reader.ReadBytes(sizeof(float) * 2);
-            float x = BitConverter.ToSingle(bytes, 0);
-            float y = BitConverter.ToSingle(bytes, sizeof(float));
-            objects.Player = new Property.Player() { Position = new SlimDX.Vector3(x, y, 0.0f)};
+            var player = new Property.Player();
+            byte[] bytes = reader.ReadBytes(Marshal.SizeOf(player));
+            player.Position = new SlimDX.Vector2(BitConverter.ToSingle(bytes, 0), BitConverter.ToSingle(bytes, sizeof(float)));
+            objects.Player = player;
         }
 
         void ReadStageData(ref Objects objects)
         {
-            byte[] bytes = reader.ReadBytes(sizeof(int) * 5);
+            var stage = new Property.Stage();
+            byte[] bytes = reader.ReadBytes(Marshal.SizeOf(stage));
             int top = BitConverter.ToInt32(bytes, 0);
             int right = BitConverter.ToInt32(bytes, sizeof(int));
             int bottom = BitConverter.ToInt32(bytes, sizeof(int) * 2);
             int left = BitConverter.ToInt32(bytes, sizeof(int) * 3);
             int limit_time = BitConverter.ToInt32(bytes, sizeof(int) * 4);
-            objects.Stage = new Property.Stage() { 
-                LimitLine = new System.Drawing.Rectangle() { X = left, Y = top, Height = bottom - top, Width = right - left },
-                LimitTime = limit_time
+            stage.LimitLine = new System.Drawing.Rectangle() { 
+                X = left,
+                Y = top,
+                Height = bottom - top,
+                Width = right - left
             };
+            stage.LimitTime = limit_time;
+            objects.Stage = stage;
         }
 
+        void ReadCollisionsData(ref Objects objects)
+        {
+            objects.Collisions = new List<Property.Collision>();
+            for (int i = 0; i < objects.Collisions.Capacity; i++)
+            {
+                var new_collision = new Property.Collision();
+                byte[] bytes = reader.ReadBytes(Marshal.SizeOf(new_collision));
+                new_collision.StartingPoint = new SlimDX.Vector2(BitConverter.ToSingle(bytes, 0), BitConverter.ToSingle(bytes, sizeof(float)));
+                new_collision.TerminatePoint = new SlimDX.Vector2(BitConverter.ToSingle(bytes, sizeof(float) * 2), BitConverter.ToSingle(bytes, sizeof(float) * 3));
+                new_collision.TypeId = BitConverter.ToInt32(bytes, sizeof(float) * 4);
+                objects.Collisions[i] = new_collision;
+            }
+        }
 
+        void ReadItemsData(ref Objects objects)
+        {
+            objects.Items = new List<Property.Item>();
+            for (int i = 0; i < objects.Items.Capacity; i++)
+            {
+                var new_item = new Property.Item();
+                byte[] bytes = reader.ReadBytes(Marshal.SizeOf(new_item));
+                new_item.Position = new SlimDX.Vector2(BitConverter.ToSingle(bytes, 0), BitConverter.ToSingle(bytes, sizeof(float)));
+                new_item.TypeId = BitConverter.ToInt32(bytes, sizeof(float) * 2);
+                objects.Items[i] = new_item;
+            }
+        }
+
+        void ReadDecolationsData(ref Objects objects)
+        {
+            objects.Decolations = new List<Property.Decolation>();
+            for (int i = 0; i < objects.Decolations.Capacity; i++)
+            {
+                var new_item = new Property.Decolation();
+                byte[] bytes = reader.ReadBytes(Marshal.SizeOf(new_item));
+
+                new_item.Position = new SlimDX.Vector3(BitConverter.ToSingle(bytes, 0), BitConverter.ToSingle(bytes, sizeof(float)), BitConverter.ToSingle(bytes, sizeof(float) * 2));
+                new_item.TypeId = BitConverter.ToInt32(bytes, sizeof(float) * 3);
+                objects.Decolations[i] = new_item;
+            }
+        }
+
+        void ReadEnemiesData(ref Objects objects)
+        {
+            objects.Enemies = new List<Property.Enemy>();
+            for (int i = 0; i < objects.Enemies.Capacity; i++)
+            {
+                var new_item = new Property.Enemy();
+                byte[] bytes = reader.ReadBytes(Marshal.SizeOf(new_item));
+                new_item.Position = new SlimDX.Vector2(BitConverter.ToSingle(bytes, 0), BitConverter.ToSingle(bytes, sizeof(float)));
+                new_item.TypeId = BitConverter.ToInt32(bytes, sizeof(float) * 2);
+                objects.Enemies[i] = new_item;
+            }
+        }
 
         public void Read(string filename, out Objects objects)
         {
@@ -76,6 +134,10 @@ namespace BinaryParser
                     CheckSumData(ref objects);
                     ReadPlayerData(ref objects);
                     ReadStageData(ref objects);
+                    ReadCollisionsData(ref objects);
+                    ReadItemsData(ref objects);
+                    ReadDecolationsData(ref objects);
+                    ReadEnemiesData(ref objects);
                     Valid = true;
                 }catch(SystemException)
                 {
