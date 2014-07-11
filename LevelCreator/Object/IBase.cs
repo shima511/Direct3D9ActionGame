48c7c4747330789fed37704fef43608b7991308a
Shima's Laptop <wiicrew@mist.ocn.ne.jp>
2014-07-11 17:54:46 +0900

    * オブジェクトの基礎インターフェースを作成

---
+++ LevelCreator/Object/IBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LevelCreator.Object
+{
+    abstract class IBase
+    {
+        public abstract void Update();
+        public virtual void InputAction(KeyEventArgs e)
+        {
+
+        }
+        public abstract void Draw(SlimDX.Direct3D9.Device dev);
+    }
+}
