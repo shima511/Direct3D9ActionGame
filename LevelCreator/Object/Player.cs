a84e6c59096097505ef0ad7430cff119bb11c22c
Shima <wiicrew@mist.ocn.ne.jp>
2014-07-12 23:48:28 +0900

    * プレイヤークラスを実装

---
+++ LevelCreator/Object/Player.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LevelCreator.Object
+{
+    sealed class Player : Model
+    {
+        public BinaryParser.Property.Player PlayerInfo { private get; set; }
+
+        public void Update()
+        {
+            _position.X = PlayerInfo.Position.X;
+            _position.Y = PlayerInfo.Position.Y;
+        }
+    }
+}
