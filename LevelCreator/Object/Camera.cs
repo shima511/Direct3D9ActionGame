5e900923747b9029288df0a393a5d6471920195e
Shima <wiicrew@mist.ocn.ne.jp>
2014-07-13 06:20:03 +0900

    * カメラに移動・ズームの機能を実装

--- LevelCreator/Object/Camera.cs
+++ LevelCreator/Object/Camera.cs
@@ -12,6 +12,7 @@ namespace LevelCreator.Object
     class Camera : IBase
     {
         Form form;
+        readonly float MoveDistance = 0.5f;
         private float _range = (float)Math.PI / 4;
         private Vector3 _eye_position = new Vector3(0.0f, 1.0f, -20.0f);
         private Vector3 _at_position = new Vector3(0.0f, 0.0f, 0.0f);
@@ -32,13 +33,61 @@ namespace LevelCreator.Object
 
         public void InputAction(KeyEventArgs e)
         {
+            int strength = 1;
+            if (e.Control)
+            {
+                strength = 5;
+            }
+            if (!e.Alt)
+            {
+                switch (e.KeyCode)
+                {
+                    case Keys.Left:
+                        _eye_position.X -= MoveDistance * strength;
+                        _at_position.X -= MoveDistance * strength;
+                        break;
+                    case Keys.Right:
+                        _eye_position.X += MoveDistance * strength;
+                        _at_position.X += MoveDistance * strength;
+                        break;
+                    case Keys.Up:
+                        _eye_position.Y += MoveDistance * strength;
+                        _at_position.Y += MoveDistance * strength;
+                        break;
+                    case Keys.Down:
+                        _eye_position.Y -= MoveDistance * strength;
+                        _at_position.Y -= MoveDistance * strength;
+                        break;
+                }
+            }
+            else
+            {
+                switch (e.KeyCode)
+                {
+                    case Keys.Up:
+                        _eye_position.Z += MoveDistance * strength;
+                        _at_position.Z += MoveDistance * strength;
+                        break;
+                    case Keys.Down:
+                        _eye_position.Z -= MoveDistance * strength;
+                        _at_position.Z -= MoveDistance * strength;
+                        break;
+                }
+            }
+        }
 
+        public void MouseAction(MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+
+            }
         }
 
         public void Draw(SlimDX.Direct3D9.Device dev)
         {
             var view_mat = Matrix.LookAtLH(EyePosition, AtPosition, UpDirection);
-            var proj_mat = Matrix.PerspectiveFovLH(_range, (float)form.Width / form.Height, 0.1f, 20.0f);
+            var proj_mat = Matrix.PerspectiveFovLH(_range, (float)form.Width / form.Height, 5.0f, 30.0f);
             dev.SetTransform(TransformState.View, view_mat);
             dev.SetTransform(TransformState.Projection, proj_mat);
         }
