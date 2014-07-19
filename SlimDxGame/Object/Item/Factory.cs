046d251558eb9960ac8d870ac4b6a0ae5b824f0d
Shima <wiicrew@mist.ocn.ne.jp>
2014-07-20 08:43:50 +0900

    * アイテムファクトリクラスを作成

---
+++ SlimDxGame/Object/Item/Factory.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlimDxGame.Object.Item
+{
+    class Factory
+    {
+        public Collision.Manager CollisionManager { private get; set; }
+        public List<Component.IUpdateObject> UpdateList { private get; set; }
+        public List<List<Component.IDrawableObject>> Layers { private get; set; }
+
+        public void Create(BinaryParser.Property.Item obj, out IBase new_item)
+        {
+            new_item = null;
+            switch (obj.TypeId)
+            {
+                case 0:
+                    new_item = new Item.Coin();
+                    break;
+                case 1:
+                    new_item = new Item.Portion();
+                    break;
+            }
+            new_item.Position2D = obj.Position;
+            new_item.OnHit += new_item_OnHit;
+        }
+
+        void new_item_OnHit(IBase obj)
+        {
+            CollisionManager.Remove(obj);
+            UpdateList.Remove(obj);
+            Layers[0].Remove(obj);
+        }
+    }
+}
