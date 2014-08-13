7df7e120c772675e2e89128cb6f0d61fe10f3b1c
Shima <wiicrew@mist.ocn.ne.jp>
2014-08-13 10:32:30 +0900

    * 必要なクラス・構造体を追加

---
+++ ScriptRW/ObjectProperty.cs
@@ -0,0 +1,9 @@
+﻿namespace ScriptRW
+{
+    public struct ObjectProperty
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string AssetPath { get; set; }
+    }
+}
