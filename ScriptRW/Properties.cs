2f9eeb58ca5abc750977e851b7e322e666a9b00f
Shima <wiicrew@mist.ocn.ne.jp>
2014-08-14 09:24:57 +0900

    * バイナリ変換機能・読み込み機能を実装

---
+++ ScriptRW/Properties.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace ScriptRW
+{
+    public struct Properties
+    {
+        public List<ObjectProperty> Decolations { get; set; }
+        public List<ObjectProperty> Items { get; set; }
+        public List<ObjectProperty> Enemies { get; set; }
+        public List<ObjectProperty> Textures { get; set; }
+    }
+}
