bfde900831b19bbb02f608344d1c88b4c4f5bf4d
Shima <wiicrew@mist.ocn.ne.jp>
2014-08-23 07:06:40 +0900

    * Oggへの依存を削除 * ライトクラスを実装中 * 装飾品ファクトリクラスを実装

---
+++ SlimDxGame/Object/Decolation/Factory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlimDxGame.Object.Decolation
+{
+    class Factory
+    {
+        public AssetContainer<Asset.Model> ModelContainer { private get; set; }
+
+        public void Create(StageRW.Property.Decolation obj, out Object.Base.Model new_item)
+        {
+            new_item = null;
+            switch (obj.TypeId)
+            {
+                case 0:
+                    new_item = new Base.Model()
+                    {
+                        Position = obj.Position,
+                        ModelAsset = ModelContainer.GetValue("Field0")
+                    };
+                    break;
+            }
+        }
+    }
+}
