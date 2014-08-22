0f13c0561df24e1b5acc20c4b1be3588fd15677a
Shima <wiicrew@mist.ocn.ne.jp>
2014-08-23 07:38:16 +0900

    * ogg読み込みクラスを削除

--- SlimDxGame/SoundLoader.cs
+++ SlimDxGame/SoundLoader.cs
@@ -36,41 +36,4 @@ namespace SlimDxGame
         }
     }
 
-    class OggFileLoader : SoundLoader
-    {
-        private void ConvertFormat(ref WaveFormat wav_format, Tsukikage.Audio.OggDecodeStream oggStream){
-            wav_format.AverageBytesPerSecond = wav_format.SamplesPerSecond * ((int)wav_format.BitsPerSample / 8);
-            wav_format.SamplesPerSecond = oggStream.SamplesPerSecond;
-            wav_format.Channels = (short)oggStream.Channels;
-            wav_format.BitsPerSample = (short)oggStream.BitsPerSample;
-            wav_format.FormatTag = WaveFormatTag.Pcm;
-            wav_format.BlockAlignment = 2;
-        }
-
-        public bool LoadFromFile(out MemoryStream stream, out WaveFormat format, string filename)
-        {
-            bool ret_val = true;
-            var s = System.IO.File.OpenRead(filename);
-            try
-            {
-                Tsukikage.Audio.OggDecodeStream oggStream = new Tsukikage.Audio.OggDecodeStream(s);
-                // フォーマットを取得
-                WaveFormat f = new WaveFormat();
-                ConvertFormat(ref f, oggStream);
-                format = f;
-
-                byte[] wmaData = new byte[oggStream.Length];
-                oggStream.Read(wmaData, 0, (int)oggStream.Length);
-                stream = new MemoryStream(wmaData);
-            }
-            catch (SystemException)
-            {
-                stream = null;
-                format = null;
-                ret_val = false;
-            }
-            s.Close();
-            return ret_val;
-        }
-    }
 }
