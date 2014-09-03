d3fa1276ddb79dbfb9ccfbb1b2485f7b7e146f23
Shima <wiicrew@mist.ocn.ne.jp>
2014-09-03 18:18:42 +0900

    * ゲーム中にポーズメニューを表示できるようにした

---
+++ SlimDxGame/PauseMenuDispatcher.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlimDxGame
+{
+    class PauseMenuDispatcher : Component.IOperableObject
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool PauseButtonPushed { get; private set; }
+
+        public PauseMenuDispatcher()
+        {
+            PauseButtonPushed = false;
+        }
+
+        public void ControllerAction(SlimDxGame.Controller controller)
+        {
+            if (controller.StartButton.IsPressed())
+            {
+                PauseButtonPushed = true;
+            }
+        }
+    }
+}
