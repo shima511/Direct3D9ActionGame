aefeca9bc2c99d48230fdb2fc41e634f9b1dfff7
Shima <wiicrew@mist.ocn.ne.jp>
2014-09-06 15:52:32 +0900

    * コメントの挿入

--- SlimDxGame/AssetContainer.cs
+++ SlimDxGame/AssetContainer.cs
@@ -19,6 +19,11 @@ namespace SlimDxGame
             this.Clear();
         }
 
+        /// <summary>
+        /// マップとして保存した名前からオブジェクトを探します。
+        /// </summary>
+        /// <param name="name">名前</param>
+        /// <returns>アセットオブジェクト</returns>
         public TYPE GetValue(string name)
         {
             TYPE new_obj = default(TYPE);
@@ -26,6 +31,11 @@ namespace SlimDxGame
             return new_obj;
         }
 
+        /// <summary>
+        /// 不正なオブジェクトが含まれていた場合、trueを返します。
+        /// </summary>
+        /// <param name="object_names">不正なオブジェクトの名前のリスト</param>
+        /// <returns>不正なオブジェクトが含まれているかどうか</returns>
         public bool IncludeInvalidObject(ref List<string> object_names)
         {
             bool include_invalid_object = false;
