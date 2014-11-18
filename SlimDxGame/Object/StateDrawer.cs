6aa4a0089d2bd2f5416cdec5c3e573defc6b980c
Shima <wiicrew@mist.ocn.ne.jp>
2014-11-18 23:30:53 +0900

    * ライフ表示をなくした

--- SlimDxGame/Object/StateDrawer.cs
+++ SlimDxGame/Object/StateDrawer.cs
@@ -11,12 +11,13 @@ namespace SlimDxGame.Object
         public bool IsActive { get; set; }
         public bool IsVisible { get; set; }
         public Asset.Font Font { private get; set; }
+        public int HighScore { private get; set; }
         Object.Player player;
         Status.Stage stage_state;
-        Object.Base.String life_str = new Base.String();
         Object.Base.String time_str = new Base.String();
         Object.Base.String score_str = new Base.String();
         Object.Base.String state_str = new Base.String();
+        Object.Base.String high_score_str = new Base.String();
 
         public StateDrawer(Status.Stage s_state, Object.Player p)
         {
@@ -25,20 +26,20 @@ namespace SlimDxGame.Object
             IsVisible = true;
             time_str.Position = new SlimDX.Vector2(Core.Game.AppInfo.Width * 2 / 6, 0);
             score_str.Position = new SlimDX.Vector2(Core.Game.AppInfo.Width * 3 / 6, 0);
-            life_str.Position = new SlimDX.Vector2(Core.Game.AppInfo.Width * 5 / 6, 0);
             state_str.Position = new SlimDX.Vector2(Core.Game.AppInfo.Width * 1 / 6, 0);
+            high_score_str.Position = new SlimDX.Vector2(0.0f, 0.0f);
         }
 
         public void Update()
         {
             time_str.Text = "TIME:" + stage_state.Time.ToString();
             score_str.Text = "SCORE:" + stage_state.Score.ToString();
-            life_str.Text = "LIFE:" + player.Life.ToString();
+            high_score_str.Text = "HIGH SCORE:" + HighScore.ToString();
             state_str.Text = player.State.ToString();
             time_str.Font = Font;
             score_str.Font = Font;
-            life_str.Font = Font;
             state_str.Font = Font;
+            high_score_str.Font = Font;
         }
 
         public void Draw3D(SlimDX.Direct3D9.Device dev)
@@ -56,7 +57,7 @@ namespace SlimDxGame.Object
         {
             time_str.Draw2D(dev);
             score_str.Draw2D(dev);
-            life_str.Draw2D(dev);
+            high_score_str.Draw2D(dev);
             DebugDraw(dev);
         }
     }
